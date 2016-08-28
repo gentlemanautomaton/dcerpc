198ddf666b8287095501f48b23da7a0ed45387b9
Joshua Sjoding <ga@josh.hey.nu>
2016-08-27 23:14:57 -0700

    Continued development of ndr64 package

--- ndr64/encode.go
+++ ndr64/encode.go
@@ -7,18 +7,31 @@ import (
 	"github.com/gentlemanautomaton/dcerpc/ndr"
 )
 
+type encInstr struct {
+	op    ndr.EncOp
+	index []int
+}
+
 // EncOpForSlice returns an NDR encoding function for the given type, which
 // must be a slice.
 func EncOpForSlice(rt reflect.Type) ndr.EncOp {
 	// FIXME: Handle multiple dimensions
 	elemOp := EncOpFor(rt.Elem())
 	return func(w ndr.Writer, s *ndr.State, v reflect.Value) {
-		w.WriteUint32(0)                 // Varying array offset, always zero in our case
-		w.WriteUint32((uint32)(v.Len())) // Varying array length, in number of elements
+		w.WriteUint64(0)                 // Varying array offset, always zero in our case
+		w.WriteUint64((uint64)(v.Len())) // Varying array length, in number of elements
 		for e := 0; e < v.Len(); e++ {
 			elemOp(w, s, v.Index(e))
 		}
 	}
+}
+
+// EncOpForConformantStruct returns an NDR64 conformant data encoding function
+// for the given type, which must be a struct.
+func EncOpForConformantStruct(rt reflect.Type) (ndr.EncOp, []int) {
+	// TODO: Figure out a decent implementation for this in the ndr package
+	//       and then create the appropriate implementation here.
+	return ndr.EncNoop, nil
 }
 
 // EncOpForStruct returns an NDR encoding function for the given type, which
@@ -53,27 +66,13 @@ func EncOpForStruct(rt reflect.Type) ndr.EncOp {
 
 // EncOpForField returns an NDR encoding function for the given field.
 func EncOpForField(rf reflect.StructField) ndr.EncOp {
+	if op := ndr.EncOpForPrimitive(rf.Type); op != nil {
+		return op
+	}
+
 	attrs := types.ParseFieldAttrList(rf.Tag.Get("idl"))
 
 	switch rf.Type.Kind() {
-	case reflect.Bool:
-		return ndr.EncBool
-	case reflect.Int8:
-		return ndr.EncInt8
-	case reflect.Uint8:
-		return ndr.EncUint8
-	case reflect.Int16:
-		return ndr.EncInt16
-	case reflect.Uint16:
-		return ndr.EncUint16
-	case reflect.Int32:
-		return ndr.EncInt32
-	case reflect.Uint32:
-		return ndr.EncUint32
-	case reflect.Int64:
-		return ndr.EncInt64
-	case reflect.Uint64:
-		return ndr.EncUint64
 	case reflect.Array:
 		return ndr.EncOpForArray(rf.Type)
 	case reflect.Slice:
@@ -94,8 +93,10 @@ func EncOpForField(rf reflect.StructField) ndr.EncOp {
 
 // EncOpFor returns an NDR encoding function for the given type.
 func EncOpFor(rt reflect.Type) ndr.EncOp {
-	// TODO: Figure out a good workaround for specifying attributes for non-fields
-	//       Perhaps they could be namelessly composed into containing structs?
+	if op := ndr.EncOpForPrimitive(rt); op != nil {
+		return op
+	}
+
 	switch rt.Kind() {
 	case reflect.Array:
 	case reflect.String:
