ae518e4c83cb669cc3809196247d9f27a050f67b
Joshua Sjoding <ga@josh.hey.nu>
2017-07-13 00:55:32 -0600

    Added client and server stubs

---
+++ client.go
@@ -0,0 +1,20 @@
+package dcerpc
+
+import "context"
+
+// Client is a DCE / RPC client that is capable of making procedure calls to
+// one or more remote servers.
+//
+// Client implements the client side of the protocol described in the
+// "DCE 1.1: Remote Procedure Call" technical standard.
+type Client struct {
+}
+
+// Invoke will run the requested remote procedure.
+//
+// TODO: Figure out a good way to pass binding and interface information in.
+// TODO: Figure out a good way to pass the RPC parameters in.
+// TODO: Return some sort of call struct (or interface)?
+func (c Client) Invoke(ctx context.Context) {
+
+}
---
+++ server.go
@@ -0,0 +1,9 @@
+package dcerpc
+
+// Server is a DCE / RPC server that is capable of receiving procedure calls
+// from a remote client.
+//
+// Server implements the server side of the protocol described in the
+// "DCE 1.1: Remote Procedure Call" technical standard.
+type Server struct {
+}
