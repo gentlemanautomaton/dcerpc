f1c8d0dfb451781a66190b9f2d1f3b3cb85958e7
Joshua Sjoding <ga@josh.hey.nu>
2017-07-13 00:56:26 -0600

    Added doc for dcerpc package

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Package dcerpc is a work-in-progress attempt to implement the
+// "DCE 1.1: Remote Procedure Call" technical standard.
+//
+// While great progress has been made, significant portions of the protocol
+// have yet to be implemented.
+//
+// This package is not yet suitable for use.
+package dcerpc
