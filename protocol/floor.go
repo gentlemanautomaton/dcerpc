65976f717077a14e65ecf99c6a8e0d3ab4d37ecf
Joshua Sjoding <ga@scjalliance.com>
2018-08-28 18:57:18 -0600

    protocol: Added marshaling for protocol towers

--- protocol/floor.go
+++ protocol/floor.go
@@ -1,7 +1,39 @@
 package protocol
 
-// Floor represents a floor in the protocol tower.
+import (
+	"encoding/binary"
+	"io"
+)
+
+// Floor represents a floor in the protocol tower. It describes one
+// layer of the protocol stack.
 type Floor struct {
 	ProtocolIdentifier []byte
 	AddressData        []byte
+}
+
+// WriteTo will write a binary representation of the protocol floor to w.
+func (f Floor) WriteTo(w io.Writer) (n int64, err error) {
+	buf := make([]byte, f.EncodedLength())
+	f.Marshal(buf)
+	n32, err := w.Write(buf)
+	return int64(n32), err
+}
+
+// Marshal marshals the protocol floor as a binary representation stored in p.
+// If len(p) is less than f.EncodedLength(), Marhsal will panic.
+func (f Floor) Marshal(p []byte) {
+	copyWithLength(f.ProtocolIdentifier, p) // Left-hand side (key)
+	p = p[2+len(f.ProtocolIdentifier):]
+	copyWithLength(f.AddressData, p) // Right-hand side (value)
+}
+
+// EncodedLength returns the total number of bytes required to encode f.
+func (f Floor) EncodedLength() int {
+	return 4 + len(f.ProtocolIdentifier) + len(f.AddressData)
+}
+
+func copyWithLength(from []byte, to []byte) {
+	binary.LittleEndian.PutUint16(to[0:2], uint16(len(from)))
+	copy(to[2:], from)
 }
--- protocol/tower.go
+++ protocol/tower.go
@@ -1,12 +1,44 @@
 package protocol
 
-import "io"
+import (
+	"encoding/binary"
+	"io"
+)
 
 // Tower is a protocol tower that contains binding information.
+//
+// Each element of a tower describes one layer of the protocol,
+// known in RPC parlance as a floor. Floors are stored in
+// ascending order.
+//
+// A particular combination of floors is called a protocol sequence.
 type Tower []Floor
 
-// Marshal will write a binary representation of the protocol tower to the
-// writer.
-func (t Tower) Marshal(w io.Writer) {
-	//s.Write
+// WriteTo will write a binary representation of the protocol tower to w.
+func (t Tower) WriteTo(w io.Writer) (n int64, err error) {
+	buf := make([]byte, t.EncodedLength())
+	t.Marshal(buf)
+	n32, err := w.Write(buf)
+	return int64(n32), err
+}
+
+// Marshal marshals the protocol tower as a binary representation stored in p.
+// If len(p) is less than t.EncodedLength(), Marhsal will panic.
+func (t Tower) Marshal(p []byte) {
+	binary.LittleEndian.PutUint16(p[0:2], uint16(len(t)))
+	offset := 2
+	for i := range t {
+		t[i].Marshal(p[offset:])
+		offset += t[i].EncodedLength()
+	}
+}
+
+// EncodedLength returns the total number of bytes required to marshal t.
+func (t Tower) EncodedLength() (length int) {
+	// 2-byte floor count plus the sum of all floors
+	length = 2
+	for i := range t {
+		length += t[i].EncodedLength()
+	}
+	return
 }
